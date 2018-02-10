61d7a8852b01e2e5d0ff518f86c774a9bb95236f
Daniel Rużewicz <d.ruzew@gmail.com>
2018-02-10 23:00:46 +0100

    Added "form" class

---
+++ SiteForPartyGuests/Models/Form.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SiteForPartyGuests.Models
+{
+    public class Form 
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool? WillAttend { get; set; }
+
+    }
+}
